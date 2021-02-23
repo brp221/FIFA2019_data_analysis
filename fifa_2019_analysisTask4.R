1ae1dc637a3faa2e97abda409544fc16e9f7be7a
Bratislav Petkovic <brp221@lehigh.edu>
2021-02-23 14:47:45 -0500

    Finished Task 4

--- fifa_2019_analysisTask4.R
+++ fifa_2019_analysisTask4.R
@@ -31,16 +31,51 @@ fifa_19$TechniqueScore <- as.integer(TechniqueScore)
 
 fifa_19_temp_no_gK<-subset(fifa_19,Position!="GK")
 
-ggplot(fifa_19_temp_no_gK[1:500,], aes(x=Position, y=AthleticScore)) +
-  geom_point()+
-  geom_smooth(method="lm")  # set se=FALSE to turnoff confidence bands
-ggplot(data = fifa_19_temp_no_gK[1:50,],aes(x= AthleticScore, y = TechniqueScore))+
-  geom_point()+
-  geom_smooth(method="lm") 
-ggplot(data = fifa_19_temp_no_gK[1:50,],aes(x= AthleticScore, y = Acceleration))+
-  geom_point()+
-  geom_smooth(method="lm") 
-ggplot(data = fifa_19_temp_no_gK[1:50,],aes(x= AthleticScore, y = Strength))+
-  geom_point()+
-  geom_smooth(method="lm") 
+#Analyze the market share of the first 100 players by nation 
+frequency_nat <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+unique_Nations <- unique(fifa_19[1:100,"Nationality"])
+for (i in 1:nrow(fifa_19[1:100,])){
+  for(k in 1:length(unique_Nations)){
+    if(fifa_19[i,"Nationality"] == unique_Nations[k]){
+      frequency_nat[k] <- frequency_nat[k] + 1
+    }
+  }
+}
+
+#market_share_nation_df <- data.frame("Nation" = unique_Nations, "Frequency" = frequency_nat)
+
+library(ggplot2)
+
+#turning Market Value into Numeric
+value_to_numeric<-function(value){
+  value<-str_remove(value,"€")
+  value<-str_remove(value,"M")
+  return(value)
+}
+ValueNum<-NULL
+fifa_19_100 <- fifa_19[1:100,]
+for(i in 1:nrow(fifa_19_100)){
+  ValueNum <-c(ValueNum,value_to_numeric(fifa_19[i,"Value"]))
+}
+fifa_19_100$ValueNum <- as.numeric(ValueNum)
+
+#Analyze the market share of the first 100 players by nation 
+value_tot_nat <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+unique_Nations <- unique(fifa_19[1:100,"Nationality"])
+for (i in 1:nrow(fifa_19_100)){
+  for(k in 1:length(unique_Nations)){
+    if(fifa_19_100[i,"Nationality"] == unique_Nations[k]){
+      value_tot_nat[k] <- value_tot_nat[k] + fifa_19_100[i,"ValueNum"]
+    }
+  }
+}
+pct <- round(value_tot_nat/sum(value_tot_nat)*100)
+unique_Nations <- paste(unique_Nations, pct) # add percents to labels
+unique_Nations <- paste(unique_Nations,"%",sep="") # ad % to labels
+
+
+# Create a basic bar
+pie(frequency_nat, labels = unique_Nations,col=rainbow(length(unique_Nations)), main="Frequency of Players by Nationality (Top 100)")
+# Create a basic bar
+pie(value_tot_nat, labels = unique_Nations,col=rainbow(length(unique_Nations)), main="Market Share of Value by Nationality (Top 100)")
 
